fb351ff559b8ee2c0c879a1b5b82636cf8a5ddd4
N3WC0DER <sawkin.nik@gmail.com>
2022-06-10 21:21:29 +0400

    Добавил CMake

---
+++ build/CMakeFiles/cmine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmine.dir/Server.cpp.o"
+  "CMakeFiles/cmine.dir/Server.cpp.o.d"
+  "CMakeFiles/cmine.dir/cmine.cpp.o"
+  "CMakeFiles/cmine.dir/cmine.cpp.o.d"
+  "CMakeFiles/cmine.dir/network/Socket.cpp.o"
+  "CMakeFiles/cmine.dir/network/Socket.cpp.o.d"
+  "CMakeFiles/cmine.dir/utils/Logger.cpp.o"
+  "CMakeFiles/cmine.dir/utils/Logger.cpp.o.d"
+  "cmine"
+  "cmine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cmine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cmine.
+# This may be replaced when dependencies are built.
